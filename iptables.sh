26fdb51fb1c7ca1a969af273067cc3e6faf310f7
histerix <histerix@gmail.com>
2021-02-12 11:10:34 +0100

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,43 +1,20 @@
 #! /bin/sh
 iptables -F
 iptables -P INPUT DROP
+iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
-iptables -A INPUT -j LOG
-iptables -A FORWARD -j LOG
-iptables -A INPUT -i lo -p all -j ACCEPT
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p icmp --icmp-type 13 -j DROP
-iptables -A INPUT -p icmp --icmp-type 17 -j DROP
-iptables -A INPUT -p icmp --icmp-type 14 -j DROP
-iptables -A INPUT -p icmp -m limit --limit 1/second -j ACCEPT
-iptables -A INPUT -m state --state INVALID -j DROP
-iptables -A FORWARD -m state --state INVALID -j DROP
-iptables -A OUTPUT -m state --state INVALID -j DROP
-iptables -A INPUT -s 169.254.0.0/16 -j DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -s 127.0.0.0/8 -j DROP
-iptables -A INPUT -s 192.168.0.0/24 -j DROP
-iptables -A INPUT -s 224.0.0.0/4 -j DROP
-iptables -A INPUT -d 224.0.0.0/4 -j DROP
-iptables -A INPUT -s 240.0.0.0/5 -j DROP
-iptables -A INPUT -d 240.0.0.0/5 -j DROP
-iptables -A INPUT -s 0.0.0.0/8 -j DROP
-iptables -A INPUT -d 0.0.0.0/8 -j DROP
-iptables -A INPUT -d 239.255.255.0/24 -j DROP
-iptables -A INPUT -d 255.255.255.255 -j DROP
-iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-iptables -A INPUT   -m recent --name portscan --rcheck --seconds 86400 -j DROP
-iptables -A FORWARD -m recent --name portscan --rcheck --seconds 86400 -j DROP
-iptables -A INPUT   -m recent --name portscan --remove
-iptables -A FORWARD -m recent --name portscan --remove
-iptables -A INPUT   -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "Portscan:"
-iptables -A INPUT   -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
-iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "Portscan:"
-iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
-iptables -A INPUT -p tcp -m tcp --dport 25 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 372 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
-iptables -A INPUT -p tcp --syn --dport 372 -m connlimit --connlimit-above 4 -j REJECT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 372 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 1514 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 3306 -m state --state NEW -j ACCEPT
