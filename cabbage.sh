2e2814aad8276430cc50a19b54436fbb9f3cfccb
histerix <histerix@gmail.com>
2021-02-13 08:07:24 +0100

    Update cabbage.sh

--- cabbage.sh
+++ cabbage.sh
@@ -22,8 +22,6 @@ echo
 }
 
 clear
-f_banner
-
 
 check_root() {
 if [ $EUID -ne 0 ]; then
@@ -39,11 +37,6 @@ fi
 check_root
 say_continue
 
-echo -e ""
-echo -e "Disabling unused filesystems"
-spinner
-sleep 2
-
 echo "install cramfs /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install freevxfs /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install jffs2 /bin/true" >> /etc/modprobe.d/CIS.conf
@@ -54,31 +47,17 @@ echo "install udf /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install vfat /bin/true" >> /etc/modprobe.d/CIS.conf
 
 clear
-f_banner
-
-echo -e ""
-echo -e "Setting Sticky bit on all world-writable directories"
-sleep 2
-spinner
 
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
 
-
 clear
-f_banner
-echo -e ""
+
 echo -e "Installing and configuring AIDE"
 
 apt-get install aide -y
 aideinit
 
 clear
-f_banner
-
-echo -e ""
-echo -e "Securing Boot Settings"
-spinner
-sleep 2
 
 chown root:root /boot/grub/grub.cfg
 chmod og-rwx /boot/grub/grub.cfg
@@ -103,12 +82,6 @@ sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/g' /etc/defa
 update-grub
 
 clear
-f_banner
-
-echo -e ""
-echo -e "Setting hosts.allow and hosts.deny"
-spinner
-sleep 2
 
 echo "ALL: 10.0.0.0/255.0.0.0" >> /etc/hosts.allow
 echo "ALL: 192.168.0.0/255.255.0.0" >> /etc/hosts.allow
@@ -123,12 +96,6 @@ chown root:root /etc/hosts.deny
 chmod 644 /etc/hosts.deny
 
 clear
-f_banner
-
-echo -e ""
-echo -e "Disabling uncommon Network Protocols"
-spinner
-sleep 2
 
 echo "install dccp /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install sctp /bin/true" >> /etc/modprobe.d/CIS.conf
@@ -136,12 +103,10 @@ echo "install rds /bin/true" >> /etc/modprobe.d/CIS.conf
 echo "install tipc /bin/true" >> /etc/modprobe.d/CIS.conf
 
 clear
-f_banner
 
 echo -e ""
-echo -e "Setting up Iptables Rules"
-spinner
-sleep 1
+echo -e "Setting up iptables"
+
 
 sh templates/iptables-CIS.sh
 cp templates/iptables-CIS.sh /etc/init.d/
@@ -149,12 +114,8 @@ chmod +x /etc/init.d/iptables-CIS.sh
 ln -s /etc/init.d/iptables-CIS.sh /etc/rc2.d/S99iptables-CIS.sh
 
 clear
-f_banner
-echo -e ""
-echo -e "Installing and configuring Auditd"
 
-spinner
-sleep 1
+echo -e "Installing and configuring Auditd"
 
 apt-get install auditd -y
 
@@ -187,40 +148,28 @@ touch /etc/at.allow
 chmod og-rwx /etc/cron.allow /etc/at.allow
 chown root:root /etc/cron.allow /etc/at.allow
 
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m We will now Create a New User for SSH Access"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo -n " Type the new username: "; read username
+echo -n "Type the new administrative user's username: "; read username
 adduser $username
 
-echo -n " Securing SSH..."
+echo -n "Securing SSH..."
 sed s/USERNAME/$username/g templates/sshd_config-CIS > /etc/ssh/sshd_config; echo "OK"
 
 runuser -u $username -- ssh-keygen -t rsa -f /home/$username/.ssh/id_rsa -q -P ""
 runuser -u $username -- touch /home/$username/.ssh/authorized_keys
 chmod 600 /home/$username/.ssh/authorized_keys
 cat /root/.ssh/authorized_keys > /home/$username/.ssh/authorized_keys
-
+clear
 service sshd restart
 
 chown root:root /etc/ssh/sshd_config
 chmod og-rwx /etc/ssh/sshd_config
 
 clear
-f_banner
-
-echo -e ""
-echo -e "Configuring PAM"
-spinner
-sleep 2
 
 cp templates/common-passwd-CIS /etc/pam.d/common-passwd
 cp templates/pwquality-CIS.conf /etc/security/pwquality.conf
 cp templates/common-auth-CIS /etc/pam.d/common-auth
-
 cp templates/login.defs-CIS /etc/login.defs
-
 useradd -D -f 30
 
 for user in `awk -F: '($3 < 1000) {print $1 }' /etc/passwd`; do
@@ -237,7 +186,7 @@ usermod -g 0 root
 sed -i s/umask\ 022/umask\ 027/g /etc/init.d/rc
 
 clear
-f_banner
+
 echo -e ""
 echo -e "Setting System File Permissions"
 spinner
@@ -268,20 +217,11 @@ chown root:root /etc/gshadow-
 chmod 600 /etc/gshadow-
 
 read ip < <(last -i root | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+') && echo ALL: $ip >> /etc/hosts.allow
-
 usermod -aG sudo $username
-
 echo $username "ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
 service sshd restart
-
 clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Securing /tmp Folder"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo -n " ¿Did you Create a Separate /tmp partition during the Initial Installation? (y/n): "; read tmp_answer
+echo -n "Do you have a separate /tmp partition? (y/n): "; read tmp_answer
 if [ "$tmp_answer" == "n" ]; then
 echo "We will create a FileSystem for the /tmp Directory and set Proper Permissions "
 spinner
@@ -294,232 +234,77 @@ cp -Rpf /tmpbackup/* /tmp/
 rm -rf /tmpbackup
 echo "/usr/tmpDISK  /tmp    tmpfs   loop,nosuid,nodev,noexec,rw  0 0" >> /etc/fstab
 sudo mount -o remount /tmp
-say_done
+
 else
-echo "Nice Going, Remember to set proper permissions in /etc/fstab"
-echo ""
-echo "Example:"
-echo ""
-echo "/dev/sda4   /tmp   tmpfs  loop,nosuid,noexec,rw  0 0 "
-say_done
+
 fi
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing Fail2Ban"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
 apt install sendmail -y
 apt install fail2ban -y
-say_done
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Configuring Fail2Ban"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo " Configuring Fail2Ban......"
-spinner
 sed s/MAILTO/$inbox/g templates/fail2ban > /etc/fail2ban/jail.local
 cp /etc/fail2ban/jail.local /etc/fail2ban/jail.conf
 /etc/init.d/fail2ban restart
-say_done
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Tuning and Securing the Linux Kernel"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo " Securing Linux Kernel"
-spinner
 echo "* hard core 0" >> /etc/security/limits.conf
 cp templates/sysctl.conf /etc/sysctl.conf; echo " OK"
 cp templates/ufw /etc/default/ufw
 sysctl -e -p
-say_done
-    
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing RootKit Hunter"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Rootkit Hunter is a scanning tool to ensure you are you're clean of nasty tools. This tool scans for rootkits, backdoors and local exploits by running tests like:
-          - MD5 hash compare
-          - Look for default files used by rootkits
-          - Wrong file permissions for binaries
-          - Look for suspected strings in LKM and KLD modules
-          - Look for hidden files
-          - Optional scan within plaintext and binary files "
-sleep 1
+
 cd rkhunter-1.4.6/
 sh installer.sh --layout /usr --install
 cd ..
 rkhunter --update
-khunter --propupd
-echo ""
-echo " ***To Run RootKit Hunter ***"
-echo "     rkhunter -c --enable all --disable none"
-echo "     Detailed report on /var/log/rkhunter.log"
-say_done
-    
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-cho -e "\e[93m[+]\e[00m Tunning bashrc, nano and Vim"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
+rkhunter --propupd
 
-echo "Tunning .bashrc......"
-spinner
 cp templates/bashrc-root /root/.bashrc
 cp templates/bashrc-user /home/$username/.bashrc
 chown $username:$username /home/$username/.bashrc
-echo "OK"
-
-echo "Tunning Vim......"
-spinner
-tunning vimrc
-echo "OK"
-
-echo "Tunning Nano......"
-spinner
-tunning nanorc
-echo "OK"
-say_done
     
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Adding Daily System Update Cron Job"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Creating Daily Cron Job"
-spinner
 job="@daily apt update; apt dist-upgrade -y"
 touch job
 echo $job >> job
 crontab job
 rm job
-say_done
     
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing PortSentry"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
 apt install portsentry -y
 mv /etc/portsentry/portsentry.conf /etc/portsentry/portsentry.conf-original
 cp templates/portsentry /etc/portsentry/portsentry.conf
 sed s/tcp/atcp/g /etc/default/portsentry > salida.tmp
 mv salida.tmp /etc/default/portsentry
 /etc/init.d/portsentry restart
-say_done
-    
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Cloning Repo and Installing Artillery"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
+
 git clone https://github.com/BinaryDefense/artillery
 cd artillery/
 python setup.py
 cd ..
-echo ""
-echo "Setting Iptable rules for artillery"
-spinner
+
 for port in 22 372 1433 8080 21 5900 53 110 1723 1337 10000 5800 44443 16993; do
 echo "iptables -A INPUT -p tcp -m tcp --dport $port -j ACCEPT" >> /etc/init.d/iptables.sh
 done
-echo ""
-echo "Artillery configuration file is /var/artillery/config"
-say_done 
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Running additional Hardening Steps"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Running Additional Hardening Steps...."
-spinner
 echo tty1 > /etc/securetty
 chmod 0600 /etc/securetty
 chmod 700 /root
 chmod 600 /boot/grub/grub.cfg
 apt purge at
 apt install -y libpam-cracklib
-echo ""
-echo " Securing Cron "
-spinner
+
 touch /etc/cron.allow
 chmod 600 /etc/cron.allow
 awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/cron.deny
-echo ""
-echo -n " Do you want to Disable USB Support for this Server? (y/n): " ; read usb_answer
-if [ "$usb_answer" == "y" ]; then
-echo ""
-echo "Disabling USB Support"
-spinner
+
 echo "blacklist usb-storage" | sudo tee -a /etc/modprobe.d/blacklist.conf
 update-initramfs -u
-echo "OK"
-say_done
-else
-echo "OK"
-say_done
-fi
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing UnHide"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Unhide is a forensic tool to find hidden processes and TCP/UDP ports by rootkits / LKMs or by another hidden technique."
-sleep 1
 apt -y install unhide
-echo ""
-echo " Unhide is a tool for Detecting Hidden Processes "
-echo " For more info about the Tool use the manpages "
-echo " man unhide "
-say_done
-
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing Tiger"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Tiger is a security tool that can be use both as a security audit and intrusion detection system"
-sleep 1
 apt -y install tiger
-echo ""
-echo " For More info about the Tool use the ManPages "
-echo " man tiger "
-say_done
-
 iptables -A INPUT -j LOG
 iptables -A FORWARD -j LOG
 
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Install PSAD"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo " PSAD is a piece of Software that actively monitors you Firewall Logs to Determine if a scan
-or attack event is in Progress. It can alert and Take action to deter the Threat
-NOTE:
-IF YOU ARE ONLY RUNNING THIS FUNCTION, YOU MUST ENABLE LOGGING FOR iptables
-iptables -A INPUT -j LOG
-iptables -A FORWARD -j LOG
-"
-echo ""
+
 echo -n " Do you want to install PSAD (Recommended)? (y/n): " ; read psad_answer
 if [ "$psad_answer" == "y" ]; then
 echo -n " Type an Email Address to Receive PSAD Alerts: " ; read inbox1
@@ -538,14 +323,6 @@ echo "OK"
 say_done
 fi
 
-clear
-f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Disabling Compilers"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "Disabling Compilers....."
-spinner
 chmod 000 /usr/bin/as >/dev/null 2>&1
 chmod 000 /usr/bin/byacc >/dev/null 2>&1
 chmod 000 /usr/bin/yacc >/dev/null 2>&1
@@ -555,19 +332,11 @@ chmod 000 /usr/bin/cc >/dev/null 2>&1
 chmod 000 /usr/bin/gcc >/dev/null 2>&1
 chmod 000 /usr/bin/*c++ >/dev/null 2>&1
 chmod 000 /usr/bin/*g++ >/dev/null 2>&1
-spinner echo ""
-echo " If you wish to use them, just change the Permissions"
-echo " Example: chmod 755 /usr/bin/gcc "
-echo " OK"
-say_done
 
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Enable Unattended Security Updates"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo -n " ¿Do you Wish to Enable Unattended Security Updates? (y/n): "; read unattended
+
+echo -n "Enable Unattended Security Updates? (y/n): "; read unattended
 if [ "$unattended" == "y" ]; then
 dpkg-reconfigure -plow unattended-upgrades
 else
@@ -576,20 +345,14 @@ fi
   
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Enable Process Accounting"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
+
 apt install acct -y
 touch /var/log/wtmp
 echo "OK"
   
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m Installing and enabling sysstat"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
+
 apt install sysstat -y
 sed -i 's/ENABLED="false"/ENABLED="true"/g' /etc/default/sysstat
 service sysstat start
@@ -598,12 +361,6 @@ say_done
   
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m ArpWatch Install"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "ArpWatch is a tool for monitoring ARP traffic on System. It generates log of observed pairing of IP and MAC."
-echo ""
 echo -n " Do you want to Install ArpWatch on this Server? (y/n): " ; read arp_answer
 if [ "$arp_answer" == "y" ]; then
 echo "Installing ArpWatch"
@@ -620,12 +377,7 @@ fi
 
 clear
 f_banner
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo -e "\e[93m[+]\e[00m GRUB Bootloader Password"
-echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-echo ""
-echo "It is recommended to set password on GRUB to prevent altering boot configuration (e.g. boot in single user mode)"
-echo ""
+
 echo -n " Do you want to set a GRUB bootloader password? (y/n): " ; read grub_answer
 if [ "$grub_answer" == "y" ]; then
 grub-mkpasswd-pbkdf2 | tee grubpassword.tmp
